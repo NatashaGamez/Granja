d9640bf7748ed365952f3999f7695ab17e7406c8
Unknown <unknown@example.com>
2021-02-22 11:29:26 -0600

    Precio

---
+++ Precios.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 11:14:31 2021
+
+@author: Carlos
+"""
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+#%%
